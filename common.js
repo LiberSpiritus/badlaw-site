5e1c30115fa05da46e83fe724f613b6e488be67e
LiberSpiritus <53408796+LiberSpiritus@users.noreply.github.com>
2021-11-09 01:45:58 +0900

    참고 링크도 복사 가능하게 보완
    그리고 첫줄에 각각 타이틀 영역이 들어가게 처리하여 정리하며 보기 쉽게 해놓음

--- common.js
+++ common.js
@@ -31,30 +31,36 @@ $(".tab-contents").not(':first').hide();
 
   }
 
-
-
-
 function copy3() {
-  var obj = document.querySelectorAll(".copy1 ul li:first-child, .copy1 ul li:last-child a[href], .copy2 ul li:first-child, .copy2 ul li:last-child a[href] ");
+  // var obj = document.querySelectorAll(".copy1 ul li:first-child, .copy1 ul li:last-child a[href], .copy2 ul li:first-child, .copy2 ul li:last-child a[href] ");
+  var obj = document.querySelectorAll(".copy1 ul li:first-child, .copy1 ul li:last-child a[href]");
+  var obj2 = document.querySelectorAll(".copy2 ul li:first-child, .copy2 ul li:last-child a[href]");
   const textArea = document.createElement('textarea');
-  document.body.appendChild(textArea);
+  document.body.appendChild(textArea); // textarea안에 데이터를 넣어 복사하기 위해 임시 생성.
   for(var i = 0; i < obj.length; i++){
-    console.log(obj[i].textContent);
+
     if(obj[i].hasAttribute("href") === false){
-        // textArea.value += obj[i].getAttribute('href');
-        if(obj[i].textContent.includes("참고설명")){
-          obj[i].textContent.replace("참고설명",'');
-        }
+      // 0번째 인덱스 일경우 공백 2번
+      if(i === 0){textArea.value += obj[0].textContent.replace(/^\s+|\s+$/gm,'') + '\r\n' + '\r\n'; i++;}
         textArea.value += obj[i].textContent.replace(/^\s+|\s+$/gm,'') + '\r\n';
+        
+        // 참고 링크 텍스트 지운후, refer-wrap a href 속성의 값을 가져와서 뿌려줌.
+        for(var a = 0; a < obj[i].childNodes.length; a++){
+          if (obj[i].childNodes[a].className === "refer-wrap") {
+            //textArea에 있는 값중 참고 설명을 공란으로 바꾸고 개행.
+            textArea.value = textArea.value.replace("참고 설명","").trim() + '\r\n';
+            // 그 이후에 참고 링크 href 속성의 값을 가져와서 textArea에 증폭 시킨다.
+            textArea.value += '참고 링크 : ' + obj[i].childNodes[a].childNodes[0].getAttribute('href') + '\r\n';
+          }
+        }        
     }
-      
-    
     if(obj[i].hasAttribute("href") === true){
-      textArea.value += obj[i].getAttribute('href') + '\r\n';
+      textArea.value += 'URL 링크 : '+ obj[i].getAttribute('href') + '\r\n';
     } 
+      // 마지막 인덱스 일경우 공백
+      if(i === obj.length-1){textArea.value += '\r\n';}
     textArea.select();
     document.execCommand('copy');
-
     
     // document.body.removeChild(textArea);
     //var range = document.createRange();
@@ -68,6 +74,24 @@ function copy3() {
     // document.execCommand("copy"); //복사
     // sel.removeRange(range); //선택 정보 삭제    
   } 
-  document.body.removeChild(textArea);
+  for(var i = 0; i < obj2.length; i++){
+    // console.log(obj2[1].textContent);
+    if(obj2[i].hasAttribute("href") === false){
+        if(obj2[i].textContent.includes("참고설명")){
+          obj2[i].textContent.replace("참고설명",'');
+        }
+        
+        // 0번째 인덱스 일경우 공백 2번
+        if(i === 0){textArea.value += obj2[0].textContent.replace(/^\s+|\s+$/gm,'') + '\r\n' + '\r\n'; i++;}
+        textArea.value += obj2[i].textContent.replace(/^\s+|\s+$/gm,'') + '\r\n';
+    }
+    
+    if(obj2[i].hasAttribute("href") === true){
+      textArea.value += 'URL 링크 : ' + obj2[i].getAttribute('href') + '\r\n';
+    } 
+    textArea.select();
+    document.execCommand('copy');
+  } 
+  document.body.removeChild(textArea); // textarea 사용할 이유가 없으므로 제거.
 
 }
